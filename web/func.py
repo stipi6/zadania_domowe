8d386e747b4e61112d7784ea00607c1a2dac2c45
Wiktoria Stpiczyńska <w.stpiczynska@gmail.com>
2023-12-28 11:51:50 +0100

    sql

--- web/func.py
+++ web/func.py
@@ -1,173 +1,89 @@
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'  # test db to nazwa pliku w którym utworzymy bazę danych
+db = SQLAlchemy(app)
+
+class Balance(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	balance = db.Column(db.Integer, nullable=False)
+
+class Product(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(180), nullable=False)
+	qty = db.Column(db.Integer, nullable=False)
+	price = db.Column(db.Integer, nullable=False)
+
+class History(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	action = db.Column(db.String(180), nullable=False)
+
+def db_create():
+	db.create_all()
+	balance = Balance.query.first()
+	if not balance:
+		initial_balance = Balance(balance=0)
+		db.session.add(initial_balance)
+		db.session.commit()
+
 class Manager:
 	def __init__(self):
-		self.warehouse = {}
-		self.history = []
-		self.balance = self.balance_read()
 		self.message = ""
-		self.history_read()
-		self.warehouse_read()
-
-	def history_read(self):
-		try:
-			if len(self.history) == 0:
-				with open("historia.txt", "r") as file:
-					for line in file:
-						item = line.strip()
-						self.history.append(item)
-		except:
-			pass
-
-	def warehouse_save(self):
-		try:
-			with open("magazyn.txt", "w") as file:
-				for k,v in self.warehouse.items():
-					for item in v:
-						file.write(f"{k}: ilosc: {item['ilosc']}, cena: {item['cena']}\n")
-		except:
-			pass
-
-	def warehouse_read(self):
-		try:
-			if len(self.warehouse) < 1:
-				with open("magazyn.txt", "r") as file:
-					for line in file:
-						line = line.rstrip()
-						k, v = line.split(": ilosc: ")
-						ilosc, cena = v.split(", cena: ")
-						self.warehouse[k] = [{'ilosc': int(ilosc), 'cena': float(cena)}]
-		except:
-			pass
-
-	def balance_read(self):
-		try:
-			with open("saldo.txt", "r") as file:
-				self.balance = float(file.read())
-		except:
-			self.balance = 0
-		return self.balance
-
-	def saldo(self, add):
-			try:
-				add = float(add)
-				if manager.balance + add < 0:
-					self.message = "saldo konta nie moze byc ujemne"
-				else:
-					self.balance += add
-					self.message = f"zmieniono saldo o {add} zl, saldo konta wynosi {manager.balance} zl"
-					self.history.append(f"zmiana salda o {add} zl. saldo konta wynosi {manager.balance} zl.")
-				with open("saldo.txt", "w") as file:
-					file.write(str(self.balance))
-			except:
-				pass
-
-	def sell(self, item, qty, price):
-		try:
-			item = item.lower()
-			qty = int(qty)
-			price = float(price)
-
-			if item not in manager.warehouse.keys():
-				self.message = "produkt nie znajduje sie w magazynie"
-			else:
-				price_is = False
-				qty_is = False
-				for record in manager.warehouse[item]:
-					if record["cena"] == price:
-						price_is = True
-						if record["ilosc"] >= qty:
-							qty_is = True
-							record["ilosc"] -= qty
-							manager.balance+=(price*qty)
-							self.message = f"sprzedano {item} w ilosci {qty} szt o cenie {price} zl"
-							manager.history.append(f"sprzedano {item} w ilosci {qty} o cenie {price} zl. "
-										f"dodano do salda {price*qty} zl")
-							manager.warehouse_save()
-							if record["ilosc"] == 0:
-								manager.warehouse[item].remove(record)
-								manager.balance += (price * qty)
-								self.message = f"sprzedano {item} w ilosci {qty} szt o cenie {price} zl"
-								manager.history.append(f"sprzedano {item} w ilosci {qty} o cenie {price} zl. "
-													   f"dodano do salda {price * qty} zl")
-								manager.warehouse_save()
-				if not price_is:
-					self.message = "produkt o takiej cenie nie znajduje sie w magazynie"
-				elif not qty_is:
-					self.message ="nie ma takiej ilosci produktu w magazynie"
-		except:
-			self.message = "wprowadzono niepoprawne dane"
-
-	def buy(self, item, qty, price):
-		try:
-			item = item.lower()
-			qty = int(qty)
-			price = float(price)
-
-			if qty * price > manager.balance:
-				self.message = "brak srodkow, saldo nie moze byc mniejsze od 0"
-
-			else:
-				if item in manager.warehouse.keys():
-					no_price = True
-					for record in manager.warehouse[item]:
-						if record["cena"] == price:
-							record["ilosc"] += qty
-							no_price = False
-							manager.balance-=(qty * price)
-							self.message= f"zakupiono produkt o nazwie {item} w ilosci {qty} o cenie {price} zl"
-							manager.history.append(f"zakupiono produkt o nazwie {item} w ilosci {qty} o cenie {price} zl. "
-										f"odjeto od salda {price*qty}")
-							manager.warehouse_save()
-
-					if no_price:
-						manager.warehouse[item].append({"ilosc":qty, "cena":price})
-						manager.balance-=(qty*price)
-						self.message = f"zakupiono produkt o nazwie {item} w ilosci {qty} o cenie {price} zl"
-						manager.history.append(f"zakupiono produkt o nazwie {item} w ilosci {qty} o cenie {price} zl. "
-									f"odjeto od salda {price*qty} zl")
-						manager.warehouse_save()
-
-
-				else:
-					manager.warehouse[item] = [{"ilosc":qty, "cena":price}]
-					manager.balance-=(qty*price)
-					self.message = f"zakupiono produkt o nazwie {item} w ilosci {qty} o cenie {price} zl"
-					manager.history.append(f"zakupiono produkt o nazwie {item} w ilosci {qty} o cenie {price} zl. "
-						f"odjeto od salda {price*qty} zl")
-					manager.warehouse_save()
-
-		except:
-			self.message = "wprowadzono niepoprawne dane"
-
-	def add_action(self, action):
-		self.history.append(action)
-
-	def history_data(self, number_start=None, number_finish=None):
-		self.message = f"wykonano lacznie {len(self.history)} akcje"
-		actions = []
-		if number_start is not None:
-			number_start = int(number_start) - 1
-		else:
-			number_start = 0
-
-		if number_finish is not None:
-			number_finish = int(number_finish) - 1
-		else:
-			number_finish = len(self.history) - 1
-
-		if number_start < 0 or number_start >= len(self.history):
-			self.message = f"numer akcji poczatkowej jest poza zakresem, wybierz numer akcji od 1 do {len(self.history)}"
-		elif number_finish < 0 or number_finish >= len(self.history):
-			self.message = f"numer akcji koncowej jest poza zakresem, wybierz numer akcji od 1 do {len(self.history)}"
-		else:
-			for index in range(number_start, number_finish + 1):
-				actions.append(f"akcja - {index + 1}: {self.history[index]}")
-
-		if len(self.history) > 0:
-			with open("historia.txt", "w") as file:
-				for line in self.history:
-					file.write(str(line) + "\n")
-
-		return actions, self.message
-
 
 manager = Manager()
+
+def update_balance(add):
+	balance = Balance.query.first().balance
+	if balance + add < 0:
+		manager.message = "saldo konta nie moze byc ujemne"
+	else:
+		Balance.query.first().balance += add
+		db.session.commit()
+		return True
+
+def sell(product: Product):
+	the_product = Product.query.filter_by(name=product.name).first()
+	if the_product:
+		if the_product.qty >= product.qty:
+			update_balance(product.price * product.qty)
+			the_product.qty -= product.qty
+			manager.message = f"sprzedano produkt o nazwie {product.name} w ilosci {product.qty} o cenie {product.price} zl"
+			history = History(action=f"sprzedano produkt o nazwie {product.name} w ilosci {product.qty} o cenie {product.price} zl. "
+					 f"dodano do salda {product.price * product.qty} zl")
+			db.session.add(history)
+			db.session.commit()
+			if the_product.qty == 0:
+				db.session.delete(the_product)
+				db.session.commit()
+		else:
+			manager.message = "nie ma takiej ilosci produktu w magazynie"
+	else:
+		manager.message = "nie ma takiego produktu w magazynie"
+
+def buy(product: Product):
+	balance = Balance.query.first().balance
+	if balance >= product.price * product.qty:
+		the_product = Product.query.filter_by(name=product.name).first()
+		if the_product is not None and product.price == the_product.price:
+			update_balance(-product.price * product.qty)
+			the_product.qty += product.qty
+			manager.message = f"zakupiono produkt o nazwie {product.name} w ilosci {product.qty} o cenie {product.price} zl"
+			history = History(action=f"zakupiono produkt o nazwie {product.name} w ilosci {product.qty} o cenie {product.price} zl. "
+					 f"odjeto od salda {product.price * product.qty} zl")
+			db.session.add(history)
+			db.session.commit()
+		elif the_product is not None and product.price != the_product.price:
+			manager.message = "ten sam produkt ma inna cene"
+		else:
+			manager.message = f"zakupiono produkt o nazwie {product.name} w ilosci {product.qty} o cenie {product.price} zl"
+			history = History(action=f"sprzedano produkt o nazwie {product.name} w ilosci {product.qty} o cenie {product.price} zl. "
+					 f"odjeto od salda {product.price * product.qty} zl")
+			db.session.add(history)
+			db.session.add(product)
+			db.session.commit()
+			update_balance(-product.price * product.qty)
+		return True
+	else:
+		manager.message = "brak wystarczajacych srodkow na zakup"
+		return False
+
--- web/main.py
+++ web/main.py
@@ -1,35 +1,51 @@
-from flask import Flask, request, render_template, redirect
-from func import manager
+from flask import Flask, render_template, request, redirect
+from flask_alembic import Alembic
+import func
+from func import manager, db
+from app import app
 
-app = Flask(__name__)
 
 @app.route("/")
 def index():
-	balance = manager.balance
+	func.db_create()
+	balance = func.Balance.query.first().balance
+	history = func.History.query.all()
+	products = func.Product.query.all()
 	message = manager.message
-	warehouse = manager.warehouse
-	return render_template('index.html', balance=balance, message=message, warehouse=warehouse)
+	return render_template('index.html', balance=balance, history=history, products=products, message=message)
 
 @app.route("/balance", methods=["POST"])
-def balance():
-	add = request.form.get("add")
-	manager.saldo(add)
+def balance_r():
+	add = int(request.form.get("add"))
+	func.update_balance(add)
+	manager.message = f"zmieniono saldo o {add} zl"
+	history = func.History(action=f"zmieniono saldo o {add} zl")
+	db.session.add(history)
+	db.session.commit()
 	return redirect("/")
 
 @app.route("/sell", methods=["POST"])
-def sell():
-	item = request.form.get("item")
-	qty = request.form.get("qty")
-	price = request.form.get("price")
-	manager.sell(item, qty, price)
+def sell_r():
+	try:
+		name = request.form.get("item")
+		qty = int(request.form.get("qty"))
+		price = int(request.form.get("price"))
+		product = func.Product(name=name, qty=qty, price=price)
+		func.sell(product)
+	except ValueError:
+		manager.message = "uzupelnij wszystkie pola"
 	return redirect("/")
 
 @app.route("/buy", methods=["POST"])
-def buy():
-	item = request.form.get("item")
-	qty = request.form.get("qty")
-	price = request.form.get("price")
-	manager.buy(item, qty, price)
+def buy_r():
+	try:
+		name = request.form.get("item")
+		qty = int(request.form.get("qty"))
+		price = int(request.form.get("price"))
+		product = func.Product(name=name, qty=qty, price=price)
+		func.buy(product)
+	except ValueError:
+		manager.message = "uzupelnij wszystkie pola"
 	return redirect("/")
 
 @app.route("/history.html", methods=["GET"])
@@ -37,15 +53,17 @@ def history_page():
 	return render_template("history.html")
 
 @app.route('/history', methods=['GET', 'POST'])
-def history():
-    actions = []
-    message = ""
-    if request.method == 'POST':
-        number_start = request.form.get('line_from', type=int)
-        number_finish = request.form.get('line_to', type=int)
-        actions, message = manager.history_data(number_start, number_finish)
-    return render_template("history.html", message=message, actions=actions)
+def history_r():
+	if request.method == 'POST':
+		number_start = request.form.get('line_from', type=int)
+		number_finish = request.form.get('line_to', type=int)
+		actions = func.History.query.order_by(func.History.id).slice(number_start, number_finish).all()
+	else:
+		actions = func.History.query.all()
+	return render_template("history.html", actions=actions)
 
+alembic = Alembic()
+alembic.init_app(app)
 
 if __name__ == "__main__":
 	app.run(debug=True)
